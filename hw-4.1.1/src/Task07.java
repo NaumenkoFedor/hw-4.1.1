0fe73cd7703e70cb9197423e99730b238985743d
FeN <feudorn@mail.ru>
2024-05-10 14:21:00 +0400

    add task 7

---
+++ hw-4.1.1/src/Task07.java
@@ -0,0 +1,13 @@
+
+public class Task07 {
+
+    public static void main(String[] args) {
+
+
+        int num = 112;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            System.out.print(bit);
+        }
+    }
+}
